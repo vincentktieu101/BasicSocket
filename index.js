920eb312c45324c6db7b43c207ef8c683c38f2d9
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-25 02:46:27 -0700

    basic working verison

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: {origin: "*" }});
+var messages = [
+  "",
+  "",
+  "",
+  "",
+  "",
+  "type",
+  "stuff",
+  "to",
+  "chat",
+  ":)",
+]
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+server.listen(3000, () => {
+  console.log("Server running...")
+})
+
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+  socket.emit("messages", messages);
+
+  socket.on("message", (message) => {
+    if (message === "/clear") {
+      messages = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "type",
+        "stuff",
+        "to",
+        "chat",
+        ":)",
+      ]
+      return;
+    }
+    console.log("new message: " + message);
+    for (let i = 0; i < messages.length - 1; i++) {
+      messages[i] = messages[i+1];
+    }
+    messages[messages.length - 1] = message;
+    socket.broadcast.emit("messages", messages);
+  })
+});
