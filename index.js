55a50352a50527e3d3654b52f03d8a191a8563a0
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-25 04:38:38 -0700

    trying again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { transport : ['websocket'] });
 var messages = [
   "hi!!!",
   "welcome",
