f5a566814f925873ae7927e11be592ee02d98ccc
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-26 02:41:47 -0700

    try checking cors origin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, { cors: {origin: "*" }});
+const io = require("socket.io")(server, { cors: {origin: "." }});
 app.use(express.static(path.join(__dirname)));
 const { loadJSON, saveJSON } = require("./fs");
 
