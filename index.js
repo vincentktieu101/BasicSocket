b1a3b592e1af638cf0ec5d262808dbd4ecb8afe8
vincentktieu101@gmail.com <vincentktieu101@gmail.com>
2021-04-25 17:38:27 -0700

    refactored some code

--- index.js
+++ index.js
@@ -3,28 +3,12 @@ const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: {origin: "*" }});
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: false})); 
 app.use(express.static(path.join(__dirname)));
 
-var messages = [
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-  "...",
-]
+var messages = [];
+for (let i = 0; i < 20; i++) {
+  messages.push("...");
+}
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -36,30 +20,20 @@ io.on("connection", (socket) => {
 
   socket.on("message", (message) => {
     if (message === "/clear") {
-      messages = [
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-        "...",
-      ]
-    } else {
-      console.log("new message: " + message);
-      for (let i = 0; i < messages.length - 1; i++) {
-        messages[i] = messages[i+1];
+      messages = [];
+      for (let i = 0; i < 20; i++) {
+        messages.push("...");
       }
-      messages[messages.length - 1] = message;
+      socket.emit("messages", messages);
+      socket.broadcast.emit("messages", messages);
+      return;
     }
+    
+    console.log("new message: " + message);
+    for (let i = 0; i < messages.length - 1; i++) {
+      messages[i] = messages[i+1];
+    }
+    messages[messages.length - 1] = message;
     socket.emit("messages", messages);
     socket.broadcast.emit("messages", messages);
   })
